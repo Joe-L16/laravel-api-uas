404bae3a49606e080b1d05c96b9411c04eb7a225
Joe L <munir.sirojoel16@gmail.com>
2025-01-22 12:19:19 +0700

    feat: :sparkles:

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-
-Route::get('/', function () {
-    return view('welcome'); // Atau halaman utama aplikasi Anda
-});
-Route::get('/books', [BookController::class, 'index']);
